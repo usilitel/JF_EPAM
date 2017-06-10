330ba811ff004f9d55423bb39227ea7a11d7fe79
user1 <usilitel00@gmail.com>
2017-06-10 17:12:52 +0300

    change paths in t01.cmd

--- src/javase01/t01/t01.cmd
+++ src/javase01/t01/t01.cmd
@@ -1,4 +1,4 @@
 cd /d %~dp0
-javac -d bin src\javase01\t01\logic\Logic.java
-javac -d bin -cp bin src\javase01\t01\main\Main.java
+javac -d bin logic\Logic.java
+javac -d bin -cp bin main\Main.java
 java -cp bin  javase01.t01.main.Main 
